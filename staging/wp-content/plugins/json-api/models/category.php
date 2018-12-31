8a7d10ed6e8cc986ba040269ce1972683276fb44
Jagannath <bhulbhal1981@gmail.com>
2018-12-31 15:28:26 +0530

    initial commit

---
+++ staging/wp-content/plugins/json-api/models/category.php
@@ -0,0 +1,28 @@
+<?php
+
+class JSON_API_Category
+{
+    public $id;          // Integer
+  public $slug;        // String
+  public $title;       // String
+  public $description; // String
+  public $parent;      // Integer
+  public $post_count;  // Integer
+  
+  public function JSON_API_Category($wp_category = null)
+  {
+      if ($wp_category) {
+          $this->import_wp_object($wp_category);
+      }
+  }
+  
+    public function import_wp_object($wp_category)
+    {
+        $this->id = (int) $wp_category->term_id;
+        $this->slug = $wp_category->slug;
+        $this->title = $wp_category->name;
+        $this->description = $wp_category->description;
+        $this->parent = (int) $wp_category->parent;
+        $this->post_count = (int) $wp_category->count;
+    }
+}
